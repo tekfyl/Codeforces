aff8f1f75ccdfcec725806493361bd9604808cc9
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 20:24:55 +0530

    AC

---
+++ BeautifulYear/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int i,n;
+        cin >> n;
+        n++;
+        int a,s,d,f;
+        while(1){
+            a=n%10; s=n%100-a; d=n%1000-a-s; f=n-a-s-d;
+            s/=10; d/=100; f/=1000;
+            if(a == s || a == d || a == f || s == d || s == f || d == f) {  n++; }
+            else { cout << n; break; }
+        }
+    return 0;
+}
