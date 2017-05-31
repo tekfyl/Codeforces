1af095319c60840a60d25054999d09550f68a6bf
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 21:37:11 +0530

    AC

--- ChatRoom/main.cpp
+++ ChatRoom/main.cpp
@@ -6,11 +6,11 @@ int main(){
     std::ios_base::sync_with_stdio(false);
         int i=0,p=0;
         string s,g("hello"); cin >> s;
-        while(i<s.size() && p != 4){
+        while(i<s.size() && p != g.size()){
             i++;
             if(s[i] == g[p]) { p++; }
         }
-        p == 4? cout << "YES" : cout << "NO";
+        p == g.size()? cout << "YES" : cout << "NO";
     return 0;
 }
 
