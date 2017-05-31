05da1e6ddc19654c64bd340596991926507c215c
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 21:44:20 +0530

    Ria ko demo

--- ChatRoom/main.cpp
+++ ChatRoom/main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(){
-    std::ios_base::sync_with_stdio(false);
-        int i=0,p=0;
+    std::ios_base::sync_with_stdio(false); 
+    int i=0,p=0;
         string s,g("hello"); cin >> s;
         while(i<s.size() && p != g.size()){
-            i++;
             if(s[i] == g[p]) { p++; }
+            i++;
         }
         p == g.size()? cout << "YES" : cout << "NO";
     return 0;
