570616d2fd4645477aa9f38df4812fec8a3cc483
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 11:27:10 +0530

    AC

---
+++ TheatreSquare/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int n,m; double a;
+        cin >> n >> m >> a;
+        cout << ceil(n/a) + ceil(m/a);
+    return 0;
+}
+
