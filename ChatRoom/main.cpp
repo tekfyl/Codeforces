17880c210c7c2747e0d6bc16ddf2269c31c18099
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 21:33:19 +0530

    AC

---
+++ ChatRoom/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int i=0,p=0;
+        string s,g("hello"); cin >> s;
+        while(i<s.size() && p != 4){
+            i++;
+            if(s[i] == g[p]) { p++; }
+        }
+        p == 4? cout << "YES" : cout << "NO";
+    return 0;
+}
+
