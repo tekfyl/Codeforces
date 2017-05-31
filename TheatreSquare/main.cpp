b72ea85526186230c39deb730540337a79f7bf5f
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-05-31 11:30:00 +0530

    AC

--- TheatreSquare/main.cpp
+++ TheatreSquare/main.cpp
@@ -6,7 +6,7 @@ int main(){
     std::ios_base::sync_with_stdio(false);
         int n,m; double a;
         cin >> n >> m >> a;
-        cout << ceil(n/a) + ceil(m/a);
+        cout << ceil(n/a) * ceil(m/a);
     return 0;
 }
 
