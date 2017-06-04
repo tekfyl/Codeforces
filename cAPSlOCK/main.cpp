03a63d482c96cbd7f051623924f97d3944f6ee60
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-04 11:22:33 +0530

    AC

---
+++ cAPSlOCK/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int i,n=0;
+        //cin >> n;
+        string s; cin >> s;
+        for(i=1; i<s.size(); i++){
+            if(s[i] > 'Z') n = 1;
+        }
+        if(n==1){
+            cout << s; return 0;
+        }
+        s[0] =  s[0] >= 'a' ? s[0]-32 : s[0];
+        for(i=1; i<s.size(); i++){
+            s[i] = s[i] <= 'Z' ? s[i]+32 : s[i];
+        }
+        cout << s;
+    return 0;
+}
+
