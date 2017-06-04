892422a0d3b7f51362f1969b6ab5212be2fc2c3b
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-05 00:46:43 +0530

    AC

---
+++ CalculatingFunction/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        long long int i,n,sum;
+        cin >> n;
+        for(i=1; i<n+1; i++){
+            sum += pow(-1,i)*i;
+        }
+        cout << sum;
+    return 0;
+}
+
