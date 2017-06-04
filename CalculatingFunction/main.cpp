cd37a2487f02d103fc3ebc39ef102aa1f94e2f21
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-05 00:56:45 +0530

    AC

--- CalculatingFunction/main.cpp
+++ CalculatingFunction/main.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 int main(){
     std::ios_base::sync_with_stdio(false);
-        long long int i,n,sum=0;
+        long long int i,n;
         cin >> n;
-        for(i=1; i<n+1; i++){
-            sum += pow(-1,i)*i;
-        }
-        cout << sum;
+        cout << (n%2 == 0? n/2 : -(n+1)/2);
     return 0;
 }
 
