558b2ed6fffc5c64dcfebf27fdc42a050c04612c
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-06 16:37:52 +0530

    StillWorking

---
+++ NEwYearCandles/main.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define PB push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define REP(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define REP1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int i,n;
+        //cin >> n;
+        int a,b,h; cin >> a >> b;
+        while(a>0){
+            h += a;
+            a /= b;
+        }
+        cout << h;
+    return 0;
+}
+
