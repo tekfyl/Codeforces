24df82b49127b408e562bd9451bced8348df29c4
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-12 08:35:10 +0530

    AC

---
+++ Games/main.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+int in;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        int n,t;
+        cin >> n;
+        vi v,b;
+        rep(i,n){
+            cin >> in; v.pb(in);
+            cin >> in; b.pb(in);
+        }
+        int sum=0;
+        for(auto c:v){
+            sum += count(all(v), c);
+        }
+        cout << sum;
+    return 0;
+}
+
