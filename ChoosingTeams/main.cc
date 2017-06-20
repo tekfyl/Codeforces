8afd35323bc8e236174e2d8bb94de90bbc8a0cd3
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-20 18:52:08 +0530

    AC

---
+++ ChoosingTeams/main.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+vi v,q,st;
+int in,i,j,k;
+int n,m,t,v1,v2;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        cin >> n >> k;
+        j=0;
+        rep(i,n) cin >> in, in+k<6 ? j++: 1;
+        cout << j/3;
+    return 0;
+}
+
