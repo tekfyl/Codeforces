088d3fa8894d8aba094155e079224cdcc05b2961
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-20 12:34:25 +0530

    AC

---
+++ IlyaandBankAccount/main.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+vi v,q,st;
+int in,i,j,k;
+int n,m,t,v1,v2;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        cin >> n;
+        if(n < 0){
+            int rem = n%10;
+            m = n/100; m*=10; m+=rem;
+            cout << max(n/10, m);
+        }
+        else cout << n;
+    return 0;
+}
+
