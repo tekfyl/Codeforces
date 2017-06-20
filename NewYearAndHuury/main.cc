3b12e47734b5d23d3cbcbf69a4d543f4f9211612
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-20 13:53:28 +0530

    Working

---
+++ NewYearAndHuury/main.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+vi v,q,st;
+int in,i,j,k;
+int n,m,t,v1,v2;
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+        cin >> n >> m;
+        m = 240 - m;
+        i=0; k=5;
+        while(k<=m && i<n){
+            i++;
+            k += 5*(i+1);
+        }
+        cout << i;
+    return 0;
+}
+
