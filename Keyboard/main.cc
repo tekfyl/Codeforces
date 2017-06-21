34f6a35e4de64fdc68f2cb15f22084f2f60da399
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-21 16:08:23 +0530

    AC

--- Keyboard/main.cc
+++ Keyboard/main.cc
@@ -31,14 +31,13 @@ char c; string r;
 int main(){
     std::ios_base::sync_with_stdio(false);
         cin >> c >> r;
-        string s[3] = { "qwertyuiop", 
-                        "asdfghjkl;", 
-                        "zxcvbnm,./"};
+        string s[3] = {"qwertyuiop", "asdfghjkl;", "zxcvbnm,./"};
         for(auto x:r){
             rep(i,3){
-            if(s[i].find(x) != -1) {
-                cout << (c == 'L' ? s[i][s[i].find(x)+1] : s[i][s[i].find(x)-1]);
-            }
+                j=s[i].find(x);
+                if(j != -1) {
+                    cout << (c == 'L' ? s[i][j+1] : s[i][j-1]);
+                }
             }
         }
     return 0;
