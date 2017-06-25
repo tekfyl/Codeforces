e3f9602608be1305b73073e2137be8bc5d540192
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-25 20:41:04 +0530

    AC

--- C/KefaAndPark/main2.cc
+++ C/KefaAndPark/main2.cc
@@ -16,14 +16,11 @@
 
 using namespace std;
 
-vi q,st;
+vi v,q,st;
 int in=0,i=0,j=0,k=0,ans=0;
 int n=0,m=0,t=0,v1=0,v2=0;
 char c; string s;
-vi cat;
-const int N = 2e5 + 5;
-vector< vi > g(N);
-vi v(N,0);
+const int N = 2e5+7; vector< vi > g(N); vi vis(N,0); vi cat;
 
 void FastIO(){
     ios_base::sync_with_stdio(0);
@@ -31,28 +28,29 @@ void FastIO(){
     cout.tie(NULL);
 }
 
-void dfs(int node, int cnt){
-    int edge = 0; v[node] = 1;
-    if(cnt>m) return; 
-    for(auto j:g[node]){
-        if(v[j] == 0){
+void dfs(int s, int cnt){
+    int edge=0;
+    vis[s] = 1;
+    if(cnt>m) return;
+    for(auto j:g[s]){
+        if(vis[j] == 0){
             edge++;
             dfs(j, cat[j]*(cnt+cat[j]));
         }
     }
-    edge == 0 ? ans++:1;
+    if(edge == 0) ans++;
 }
 
 int main(){
     FastIO();
         cin >> n >> m;
         cat.pb(0);
-        rep(i,n) cin >> in, cat.pb(in);
+        rep1(i,n) cin >> in, cat.pb(in);
         rep(i,n-1){
-            cin >> v1 >> v2; 
+            cin >> v1 >> v2;
             g[v1].pb(v2); g[v2].pb(v1);
         }
-        dfs(1, cat[1]);
+        dfs(1,cat[1]);
         cout << ans;
     return 0;
 }
