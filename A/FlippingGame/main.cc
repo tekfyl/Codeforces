86e569a77a072b6763e37d01a265088411d907ea
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-26 18:39:27 +0530

    AC

---
+++ A/FlippingGame/main.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+vi v,q,st;
+int in=0,i=0,j=0,k=0,ans=0;
+int n=0,m=0,t=0,v1=0,v2=0;
+char c; string s;
+const int N = 2e5+7; vector< vi > g(N); vi vis(N,0);
+
+void FastIO(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+    FastIO();
+        cin >> n;
+        rep(i,n) cin >> in, v.pb(in);
+        rep(i,n){
+            rep(j,n){
+                st=v;
+                for(k=i; k<=j; k++){
+                    st[i] ^= 1;
+                }
+                q.pb(count(all(st), 1));
+            }
+        }
+        cout << *mx(all(q));
+    return 0;
+}
+
