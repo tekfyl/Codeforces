df70030677096e92f222f00b1ee3b6b11eb58629
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-29 13:03:10 +0530

    AC

---
+++ CD/TanyaAndToys/main.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<LL>
+#define vvi vector< vi >
+#define mk make_pair
+#define ii pair <int, int>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+
+using namespace std;
+
+vi v,q,st;
+int in=0,i=0,j=0,k=0,ans=0;
+int n=0,m=0,t=0,v1=0,v2=0;
+char c; string s;
+const int N = 2e5+7; 
+vvi g(N); 
+vi vis(N,0);
+vi app(N,0);
+vi dep(N,0);
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+        cin >> n >> m;
+        ll sum=0;
+        map<int,int> hash;
+        rep1(i,n){
+            cin >> in;
+            hash[in] = 1;
+        }
+        for(j=1; ;j++){
+            cin >> in;
+            if(hash[j] == 1) continue;
+            else{
+                sum += j;
+                if(sum > m) break;
+                hash[j] = 1;
+                ans++;
+                v.pb(j);
+            }
+        }
+        cout << ans << endl;
+        for(auto c:v) cout << c << " ";
+    return 0;
+}
+
