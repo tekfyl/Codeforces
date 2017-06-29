a6fee427c822374fb6b9106f945fcd268766f3b2
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-06-30 01:22:28 +0530

    AC

--- AB/Thor/main.cc
+++ AB/Thor/main.cc
@@ -1,46 +1,70 @@
 #include <bits/stdc++.h>
 
 #define all(c) c.begin(), c.end()
-#define LL long long
+#define ll long long
 #define pb push_back
 #define vi vector<int>
-#define vll vector<LL>
+#define vll vector<ll>
+#define vvi vector< vi >
 #define mk make_pair
 #define ii pair <int, int>
 #define fi first
 #define se second
+#define mx max_element
+#define mn min_element
 #define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
 #define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
 
 using namespace std;
 
-vi v;
-int in;
+vi v,q,st;
+int in=0,i=0,j=0,k=0,ans=0;
+int n=0,m=0,t=0,v1=0,v2=0;
+char c; string s;
+int N = 300003, M = 2e5+1; 
+vvi g(N); 
+vi vis(N,0);
+vi app(N,0);
+vi dep(N,0);
+
 
 int main(){
-    std::ios_base::sync_with_stdio(false);
-        int n,t,m,a,k=0;
+    ios_base::sync_with_stdio(0);
         cin >> n >> m;
-        vi f(n,0);
+        int mark[300003] = {0};
+        list<int> e[300003];
+        int event = 0,limit=0;
         rep(i,m){
-            int sum = 0;
             cin >> t;
-            if(t == 1){
-                cin >> in; in--;
-                f[in]++; v.pb(in);
+            switch(t){
+                case 1:
+                    cin >> j;
+                    ans++;
+                    e[j].pb(++event);   
+                    break;
+                case 2:
+                    cin >> j;
+                    while(!e[j].empty()){
+                        if(mark[e[j].back()] == 0){
+                            mark[e[j].back()] = 1;
+                            ans--;
+                        }
+                        e[j].pop_back();
+                    }
+                    break;
+                case 3:
+                    cin >> t;
+                   // for(auto d:Q) cout << d.fi << " " << d.se << endl;
+                    while(limit < t){
+                        ++limit;
+                        if(mark[limit] == 0){
+                            mark[limit] = 1;
+                            ans--;
+                        }
+                    }
+                    break;
             }
-            else if(t == 2){
-                cin >> in; in--;
-                f[in] = 0;
-            }
-            else {
-                cin >> in;
-                for(int i=0; i<in; i++){
-                    f[v[i]] = 0;
-                }
-            }
-            for(auto c:f) sum += c;
-            cout << sum << endl;
+            cout << ans << endl;
         }
     return 0;
 }
