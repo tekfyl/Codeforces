bddeac0fc2b8ad380cb86b8e3874aa4b49853dd3
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-06 09:25:44 +0530

    AC

--- AB/FreeCash/main.cc
+++ AB/FreeCash/main.cc
@@ -34,7 +34,7 @@ int main(){
     rep(i,n-1){
         cin >> m >> t;
         if(j == m && k == t) in++;
-        else ans = max(ans,in);
+        else in=1,ans = max(ans,in);
         j = m; k = t;
     }
     cout << ans;
