950e7f8e26e999d88a104a3b3231a6b3969101e5
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2018-01-28 21:51:59 +0530

    AC

---
+++ Garden/main.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector< vi >
+#define vvl vector< vector<ll> >
+#define mk make_pair
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    int ans = INT_MAX;
+    while(n--){
+        int i;
+        cin >> i;
+        if(k%i == 0){
+            ans = min(ans, k/i);
+        }
+    }
+    cout << ans;
+    return 0;
+}
+
