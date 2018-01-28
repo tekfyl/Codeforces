0c169aae34730635a9d33cb81efe04cf06d7d04c
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2018-01-28 22:00:12 +0530

    AC

---
+++ CurrencySystemInGeraldion/main.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector< vi >
+#define vvl vector< vector<ll> >
+#define mk make_pair
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 1;
+    while(n--){
+        int i; cin >> i;
+        if(i == 1) ans = -1; 
+    }
+    cout << ans;
+    return 0;
+}
+
